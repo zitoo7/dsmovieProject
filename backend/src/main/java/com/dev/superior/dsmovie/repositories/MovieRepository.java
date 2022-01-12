1843657613f2f0e684a8f10e1c8bad84910b633f
zitoo7 <zitto_7@hotmail.com>
2022-01-12 20:15:35 +0000

    Find movies

---
+++ backend/src/main/java/com/dev/superior/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.dev.superior.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.superior.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
